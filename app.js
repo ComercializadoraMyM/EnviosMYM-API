07857d4eb776c64cec66ef0eee4673abc2d76b5c
mariahv9 <mafehv1999@hotmail.com>
2021-03-23 11:52:11 -0500

    Adicion

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexCliente = require('./routes/cliente.js');
 var indexGuia = require('./routes/guia.js');
 var indexTracking = require('./routes/tracking.js');
 
 var app = express();
-
+app.use(cors());
 app.set("port", process.env.port || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -19,10 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 app.use('/api/clientes', indexCliente);
 app.use('/api/guias', indexGuia);
 app.use('/api/trackings', indexTracking);
